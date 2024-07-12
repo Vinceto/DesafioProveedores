a202c6d2bc926be80df85ba5bc59119979771d41
ELIANA AROS <heliana.aros@gmail.com>
2024-07-11 21:54:08 -0400

    fix datatable

--- src/main/webapp/assets/js/scripts.js
+++ src/main/webapp/assets/js/scripts.js
@@ -1,12 +1,16 @@
 $('#proveedoresTable').DataTable({
     "paging": true,
     "pageLength": 10,
-    "lengthMenu": [10, 25, 50, 75, 100],  // Opcional: permite seleccionar diferentes cantidades por página
+    "lengthMenu": [10, 25, 50, 75, 100],
     "language": {
         "emptyTable": "No se encontraron proveedores",
         "info": "Mostrando _START_ a _END_ de _TOTAL_ proveedores",
         "infoEmpty": "Mostrando 0 a 0 de 0 proveedores",
         "infoFiltered": "(filtrado de _MAX_ proveedores en total)",
+        "lengthMenu": "Mostrar _MENU_ proveedores",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
         "zeroRecords": "No se encontraron proveedores coincidentes",
         "paginate": {
             "first": "Primero",
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -57,8 +57,8 @@
         <% } %>
     </div>
     <%@ include file="footer.jsp" %>
-    <script><%@ include file="assets/js/scripts.js" %></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <!-- DataTables JS -->
+    <script src="assets/js/jquery-3.6.0.js"></script>
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
+    <script src="assets/js/scripts.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
